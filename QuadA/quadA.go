8509262f2c60dd5d70317bb9c32f66fc4c138393
elndour <ndourffaalloouu@gmail.com>
2023-01-15 15:48:34 +0000

    Quad1

---
+++ QuadA/quadA.go
@@ -0,0 +1,62 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func QuadA(x, y int) {
+	// largeur > longueur
+	if y > x {
+		for i := 0; i < x; i++ {
+			if i == 0 || i == x-1 {
+				z01.PrintRune('o')
+			} else {
+				z01.PrintRune('-')
+			}
+		}
+		z01.PrintRune('\n')
+		for j := 0; j < y-2; j++ {
+			z01.PrintRune('|')
+			for k := 0; k < x-2; k++ {
+				z01.PrintRune(' ')
+			}
+			z01.PrintRune('|')
+			z01.PrintRune('\n')
+		}
+		if y > 1 {
+			for i := 0; i < x; i++ {
+				if i == 0 || i == x-1 {
+					z01.PrintRune('o')
+				} else {
+					z01.PrintRune('-')
+				}
+			}
+			z01.PrintRune('\n')
+		}
+	} else {
+		for i := 0; i < x; i++ {
+			if i == 0 || i == x-1 {
+				z01.PrintRune('o')
+			} else {
+				z01.PrintRune('-')
+			}
+		}
+		z01.PrintRune('\n')
+		for j := 0; j < y-2; j++ {
+			z01.PrintRune('|')
+			for k := 0; k < x-2; k++ {
+				z01.PrintRune(' ')
+			}
+			z01.PrintRune('|')
+			z01.PrintRune('\n')
+		}
+		if y > 1 {
+			for i := 0; i < x; i++ {
+				if i == 0 || i == x-1 {
+					z01.PrintRune('o')
+				} else {
+					z01.PrintRune('-')
+				}
+			}
+			z01.PrintRune('\n')
+		}
+	}
+}
---
+++ QuadD/quadD.go
@@ -0,0 +1,84 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func QuadD(x, y int) {
+	if y > x {
+		// y > x et x==1
+		if x == 1 {
+			z01.PrintRune('A')
+			z01.PrintRune('\n')
+			for j := 0; j < y-2; j++ {
+				z01.PrintRune('B')
+				z01.PrintRune('\n')
+			}
+			z01.PrintRune('A')
+			z01.PrintRune('\n')
+		} else {
+			// y > x et x !=1
+			for i := 0; i < x; i++ {
+				if i == 0 {
+					z01.PrintRune('A')
+				} else if i == x-1 {
+					z01.PrintRune('C')
+				} else {
+					z01.PrintRune('B')
+				}
+			}
+			z01.PrintRune('\n')
+			for j := 0; j < y-2; j++ {
+				z01.PrintRune('B')
+				for k := 0; k < x-2; k++ {
+					z01.PrintRune(' ')
+				}
+				z01.PrintRune('B')
+				z01.PrintRune('\n')
+			}
+			// affichage sur une deuxième ligne
+			if y > 1 {
+				for i := 0; i < x; i++ {
+					if i == 0 {
+						z01.PrintRune('A')
+					} else if i == x-1 {
+						z01.PrintRune('C')
+					} else {
+						z01.PrintRune('B')
+					}
+				}
+				z01.PrintRune('\n')
+			}
+		}
+		// x > y
+	} else {
+		for i := 0; i < x; i++ {
+			if i == 0 {
+				z01.PrintRune('A')
+			} else if i == x-1 {
+				z01.PrintRune('C')
+			} else {
+				z01.PrintRune('B')
+			}
+		}
+		z01.PrintRune('\n')
+		for j := 0; j < y-2; j++ {
+			z01.PrintRune('B')
+			for k := 0; k < x-2; k++ {
+				z01.PrintRune(' ')
+			}
+			z01.PrintRune('B')
+			z01.PrintRune('\n')
+		} // AFFICHAGE DE LE DEUXIÉME LIGNE
+		if y > 1 {
+			for i := 0; i < x; i++ {
+				if i == 0 {
+					z01.PrintRune('A')
+				} else if i == x-1 {
+					z01.PrintRune('C')
+				} else {
+					z01.PrintRune('B')
+				}
+			}
+			z01.PrintRune('\n')
+		}
+	}
+}
